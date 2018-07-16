f9fb46b55a8a5d98228e2eb7fa00687bf7cb5183
zc3212521 <827058891@qq.com>
2018-07-16 18:05:31 +0800

    init 5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/zc3212521/react-ssr.git',
       path : '/var/www/test',
-      'post-deploy' : 'cnpm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
       user : 'root',
