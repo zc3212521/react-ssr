d9e725d9454fb8b101eb393cc5de1ccac628e7fa
zc3212521 <827058891@qq.com>
2018-07-23 16:17:47 +0800

    测试修改5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
         {
             name: 'test',
             script: './server/index.js',
-            "instances": "-1",
-            "exec_mode": "cluster",
+            // "instances": "-1",
+            // "exec_mode": "cluster",
             "max_memory_restart": "2000M",
             env: {
                 NODE_ENV: 'development'
