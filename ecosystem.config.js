e726c5b8704aaa5418a50f0073c420ec33f3175a
zc3212521 <827058891@qq.com>
2018-07-23 16:15:48 +0800

    测试修改4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,51 +1,54 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
 
-    // First application
-    {
-      name      : 'test',
-      script    : './server/index.js',
-      env: {
-          NODE_ENV: 'development'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    },
-    // Second application
-    // {
-    //   name      : 'WEB',
-    //   script    : 'web.js'
-    // }
-  ],
+        // First application
+        {
+            name: 'test',
+            script: './server/index.js',
+            "instances": "-1",
+            "exec_mode": "cluster",
+            "max_memory_restart": "2000M",
+            env: {
+                NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        },
+        // Second application
+        // {
+        //   name      : 'WEB',
+        //   script    : 'web.js'
+        // }
+    ],
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'root',
-      host : '192.168.2.171',
-      ref  : 'origin/master',
-      repo : 'https://github.com/zc3212521/react-ssr.git',
-      path : '/var/www/test',
-      'post-deploy' : 'git pull && cnpm install && pm2 startOrRestart ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'root',
-      host : '192.168.2.171',
-      ref  : 'origin/master',
-      repo : 'git@github.com:zc3212521/react-ssr.git',
-      path : '/var/www/dev',
-      'post-deploy' : 'cnpm install && pm2 startOrReload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
+    /**
+     * Deployment section
+     * http://pm2.keymetrics.io/docs/usage/deployment/
+     */
+    deploy: {
+        production: {
+            user: 'root',
+            host: '192.168.2.171',
+            ref: 'origin/master',
+            repo: 'https://github.com/zc3212521/react-ssr.git',
+            path: '/var/www/test',
+            'post-deploy': 'git pull && cnpm install && pm2 startOrRestart ecosystem.config.js --env production'
+        },
+        dev: {
+            user: 'root',
+            host: '192.168.2.171',
+            ref: 'origin/master',
+            repo: 'git@github.com:zc3212521/react-ssr.git',
+            path: '/var/www/dev',
+            'post-deploy': 'cnpm install && pm2 startOrReload ecosystem.config.js --env dev',
+            env: {
+                NODE_ENV: 'dev'
+            }
+        }
     }
-  }
 };
